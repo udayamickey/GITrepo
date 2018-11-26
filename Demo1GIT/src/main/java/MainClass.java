99b9076962f8192eb46eec3a178a078d28959a80
udayamickey <44608067+udayamickey@users.noreply.github.com>
2018-11-26 10:00:43 +0530

    Update MainClass.java

--- Demo1GIT/src/main/java/MainClass.java
+++ Demo1GIT/src/main/java/MainClass.java
@@ -4,9 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		display();
-		DummyClass dummyObj=new DummyClass();
-		dummyObj.show();
-
+		
 	}
 	public static void display() {
 		System.out.println("You are in display method");
